de129da3054e4a734ae9cbc51fd9590dffc92085
Remi <Remi@REMILAPTOP>
2017-04-11 18:09:40 +0200

    hex, mainWnd

--- Hex/Hex.cs
+++ Hex/Hex.cs
@@ -19,43 +19,84 @@ namespace WpfApplication1
 {
     public class Hex : Shape
     {
-        public Polygon p = new Polygon();
+        public static readonly DependencyProperty SizeProperty = DependencyProperty.Register("Size", typeof(Double), typeof(Hex));
+       
         public PointCollection points = null;
         public int i = 10;
-        private Brush bru = Brushes.RoyalBlue;
-        
-        public Hex(Point center, double Size, Brush b, Canvas c=null, MouseButtonEventHandler HexFunc = null) 
+        public Brush bru = Brushes.RoyalBlue;
+        public Brush stro = Brushes.Black;
+        private double size;
+        public Hex() { }
+        public Hex(Point center, double Size, Brush b, Canvas c = null, MouseButtonEventHandler HexFunc = null)
         {
+            size = Size;
             double sqrt = Size * Math.Sqrt(3) / 2;
             points = new PointCollection(6);
-            points.Add(new Point(Convert.ToInt32(center.X - Size / 2), Convert.ToInt32(center.Y + sqrt)));
-            points.Add(new Point(Convert.ToInt32(center.X + Size / 2), Convert.ToInt32(center.Y + sqrt)));
-            points.Add(new Point(Convert.ToInt32(center.X + Size), Convert.ToInt32(center.Y)));
-            points.Add(new Point(Convert.ToInt32(center.X + Size / 2), Convert.ToInt32(center.Y - sqrt)));
-            points.Add(new Point(Convert.ToInt32(center.X - Size / 2), Convert.ToInt32(center.Y - sqrt)));
-            points.Add(new Point(Convert.ToInt32(center.X - Size), Convert.ToInt32(center.Y)));
-            p.Stroke = Brushes.Black;
-            p.Fill = b; 
-            p.StrokeThickness = 1.7;
-            p.HorizontalAlignment = HorizontalAlignment.Left;
-            p.VerticalAlignment = VerticalAlignment.Center;
-            p.Points = points;
-            if(c!=null) c.Children.Add(p);
-            if (HexFunc != null) p.MouseDown += HexFunc;
-            else p.MouseDown += Klik;
+            points.Add(new Point(center.X - Size / 2, center.Y + sqrt));
+            points.Add(new Point(center.X + Size / 2, center.Y + sqrt));
+            points.Add(new Point(center.X + Size, center.Y));
+            points.Add(new Point(center.X + Size / 2, center.Y - sqrt));
+            points.Add(new Point(center.X - Size / 2, center.Y - sqrt));
+            points.Add(new Point(center.X - Size, center.Y));
+
+            bru = b;
+            //if (c != null) c.Children.Add(p);
+            if (c != null) c.Children.Add(this);
+            if (HexFunc != null) this.MouseDown += HexFunc;
+            else MouseDown += Klik;
+
+            MouseEnter += Highlight;
+            MouseLeave += Default;
+
+            Fill = bru;                         //kolor środka           
+            Stroke = stro;                      //kolor ramki
+            StrokeThickness = 1.5;              //grubośc ramki
+
+        }
+
+        private void Default(object sender, MouseEventArgs e)
+        {
+            Fill = bru;
+            //StrokeThickness /= 1.2;
+            Stroke = stro;
+        }
+
+        private void Highlight(object sender, MouseEventArgs e)
+        {
+            Stroke = Brushes.Green;
+            //StrokeThickness *= 1.2;
         }
 
         private void Klik(object sender, MouseButtonEventArgs e)
         {
-            p.Fill = Brushes.Green;
-            p.Fill = bru;
+           
+            bru = Fill = Brushes.Green;
         }
+
 
         protected override Geometry DefiningGeometry
         {
             get
             {
-                return p.RenderedGeometry;
+                #region nietykać
+                var g = new PathGeometry();
+                var figure = new PathFigure();
+                figure.StartPoint = points[0];
+               // figure.Segments.Add(new LineSegment(points[0], true));
+                figure.Segments.Add(new LineSegment(points[1], true));
+                figure.Segments.Add(new LineSegment(points[2], true));
+                figure.Segments.Add(new LineSegment(points[3], true));
+                figure.Segments.Add(new LineSegment(points[4], true));
+                figure.Segments.Add(new LineSegment(points[5], true));
+                
+                figure.IsFilled = true;
+                figure.IsClosed = true;
+
+                g.Figures.Add(figure);
+                #endregion
+
+                
+                return g;
             }
         }
     }
@@ -63,16 +104,87 @@ namespace WpfApplication1
     public class HexGrid : Canvas
     {
         public Hex[,] hexG = null;
+        public Hex center;
 
-        public HexGrid(int r, int c, Brush b, int Size,Canvas Canv, MouseButtonEventHandler HexFunc = null) : base()
+        public HexGrid(int r, int c, Brush b, int Size, Canvas Canv, MouseButtonEventHandler HexFunc = null) : base()
         {
-           
+
             int sqrt2 = Convert.ToInt32(Size * Math.Sqrt(3) / 2);
-            Hex[,] hexgrid = new Hex[30, 50];
+            Hex[,] hexgrid = new Hex[c, r];
             for (int j = 0; j < hexgrid.GetLength(1); ++j)
                 for (int i = 0; i < hexgrid.GetLength(0); ++i)
-                    hexgrid[i, j] = new Hex(new Point(3 * i * Size + 3 * (j % 2) * Size / 2, j * sqrt2), Size, b, Canv);
-            
+                    hexgrid[i, j] = new Hex(new Point(Size + 3 * i * Size + 3 * (j % 2) * Size / 2, Size + j * sqrt2), Size, b, Canv);
+            hexG = hexgrid;
+
         }
     }
+
+    public class HexagonalHexGrig : Canvas
+    {
+        public Hex[][] hexG;
+        public Hex Center;
+
+        private int size;
+        public HexagonalHexGrig(int n, Brush b, int Size, Canvas Canv, Point Center) : base()
+        {
+            //this.Center = Center;
+            size = n;
+            Polygon p = new Polygon();
+            p.Points = new PointCollection { Center, new Point(Center.X + 3, Center.Y), new Point(Center.X + 3, Center.Y + 3), new Point(Center.X, Center.Y + 3) };
+            p.Fill = Brushes.Red;
+
+            Canv.Children.Add(p);
+            double sqrt = Size * Math.Sqrt(3) / 2;
+            hexG = new Hex[2 * n - 1][];
+            for (int i = 0; i < n; i++)
+            {
+                hexG[i] = new Hex[n + i];
+                hexG[hexG.Length - 1 - i] = new Hex[n + i];
+            }
+            for (int i = 0; i < n - 1; i++)
+                for (int j = 0; j < hexG[i].Length; j++)
+                {
+                    hexG[i][j] = new Hex(new Point(Center.X - (n - 1 - i) * Size * 3 / 2, Center.Y - (n - 1 + i * 2) * sqrt + 2 * j * sqrt + i % 2 * sqrt + 2 * (i / 2 * sqrt)), Size, b, Canv);
+                    hexG[hexG.Length - 1 - i][j] = new Hex(new Point(Center.X + (n - 1 - i) * Size * 3 / 2, Center.Y - (n - 1 + i * 2) * sqrt + 2 * j * sqrt + i % 2 * sqrt + 2 * (i / 2 * sqrt)), Size, b, Canv);
+                }
+            for (int i = 0; i < 2 * n - 1; i++)
+            {
+                hexG[n - 1][i] = new Hex(new Point(Center.X, Center.Y + 2 * (-n + i + 1) * sqrt), Size, b, Canv);
+            }
+        }
+        public Hex this[int x, int y, int z]
+        {
+            get
+            {
+
+                try
+                {
+                    return hexG[size - 1 + x][x > 0 ? size + z - 1 : size - y - 1];
+                }
+                catch (Exception ex)
+                {
+                    if (ex.Message == "Index was outside the bounds of the array.")
+                    {
+                        MessageBox.Show("Pod podanym indeksem nie isnieje żadne pole.\n Zwracam cnetrum.");
+                        return this[0, 0, 0];
+                    }
+                    else
+                        MessageBox.Show("Wystąpił nieznany błąd. \nAplikacja moze teraz nie działać poprawnie.");
+                    return null;
+                }
+            }
+        }
+    }
+
+
+   
 }
+
+            
+
+         
+
+    // more properties
+
+    
+
--- Hex/MainWindow.xaml.cs
+++ Hex/MainWindow.xaml.cs
@@ -13,16 +13,67 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Hex
+namespace WpfApplication1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        Color actual = Colors.Chartreuse;
+        HexagonalHexGrig hhg;
         public MainWindow()
         {
             InitializeComponent();
+            
+            
+        }
+
+
+
+        private void hexdupa(object sender, RoutedEventArgs e)
+        {
+            Canv.Children.RemoveRange(0, Canv.Children.Count );
+            int size = 25;
+            hhg = new HexagonalHexGrig(7, Brushes.Purple, size, Canv, new Point(400,350));
+        }
+
+        private void dupaClk(object sender, RoutedEventArgs e)
+        {
+            
+            hhg = null;
+            Canv.Children.RemoveRange(0, Canv.Children.Count);
+            int size = 40;
+            HexGrid hg = new HexGrid(18, 5, Brushes.Chocolate, size, Canv, null);
+        }
+
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox c = sender as ComboBox;
+            actual = (Color)ColorConverter.ConvertFromString((c.SelectedItem as ComboBoxItem).Content as string);
+
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            if (hhg == null) return;
+            int x;
+            int y;
+            int z;
+            if (int.TryParse(textBox.Text, out x) &&
+            int.TryParse(textBox1.Text, out y) &&
+            int.TryParse(textBox2.Text, out z))
+            {
+                if (x + y + z == 0)
+                {
+                    hhg[x, y, z].Fill = hhg[x, y, z].bru = new SolidColorBrush(actual);
+
+                }
+                else
+                    MessageBox.Show("Podane indeksy nie sumuja się do zera.");
+            }
+            else
+                MessageBox.Show("Indeks musi być liczbą");
         }
     }
 }
